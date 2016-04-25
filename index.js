6dc1e2c032d390411528f53d1d941cd22ac004dd
Matt Brennan <mattyb1000@gmail.com>
2016-04-25 18:58:00 +0100

    better drawing madel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import range from 'lodash.range';
 import flatMap from '@quarterto/flatmap';
+import flatMapUniq from '@quarterto/flat-map-uniq';
 
 const struct = (...fields) => class {
 	constructor(...args) {
@@ -11,6 +12,23 @@ const struct = (...fields) => class {
 }
 
 class Point extends struct('x', 'y') {
+	static points = {};
+
+	constructor(...args) {
+		super(...args);
+		// hax for Point(1,1) === Point(1,1)
+		const [x, y] = args;
+		if(Point.points[x]) {
+			if(Point.points[x][y]) {
+				return Point.points[x][y];
+			}
+
+			Point.points[x][y] = this;
+		} else {
+			Point.points[x] = {[y]: this};
+		}
+	}
+
 	static from({x, y}) {
 		return new Point(x, y);
 	}
@@ -23,34 +41,51 @@ class Point extends struct('x', 'y') {
 	}
 }
 
+class BoundingGroup extends struct('children') {
+	pixels() {
+		return flatMapUniq(this.children, child => child.pixels());
+	}
+}
+
+class BoundingRect extends struct('origin', 'opposite') {
+	pixels() {
+		return flatMap(range(this.origin.x, this.opposite.x), x =>
+			range(this.origin.y, this.opposite.y).map(y => Point.from({x,y}))
+		);
+	}
+}
+
 class Sprite extends struct('origin', 'palette') {
 	pixels = [];
 
 	get boundingBox() {
-		return [
+		return new BoundingRect(
 			this.origin,
 			this.origin.add({
 				x: this.pixels[0].length,
 				y: this.pixels.length,
 			})
-		]
-	}
-
-	draw(ctx) {
-		this.pixels.forEach((rowPixels, row) => {
-			rowPixels.forEach((pixel, column) => {
-				ctx.fillStyle = this.palette[pixel];
-				ctx.fillRect(this.origin.x + column, this.origin.y + row, 1, 1);
-			})
-		})
-	}
-}
-
-class BoundingBox extends struct('origin', 'other') {
-	get pixels() {
-		return flatMap(range(this.origin.x, this.other.x), x =>
-			range(this.origin.y, this.other.y).map(y => Point.from({x, y}))
 		);
+	}
+
+	lastBoundingBox = null;
+
+	changedPixels() {
+		if(this.lastBoundingBox) {
+			const group = new BoundingGroup(this.lastBoundingBox, this.boundingBox);
+			return group.pixels();
+		}
+
+		return this.boundingBox.pixels();
+	}
+
+	getPixel(x, y) {
+		const rx = x - this.origin.x;
+		const ry = y - this.origin.y;
+		if(rx < 0 || ry < 0 || rx >= this.pixels[0].length || ry >= this.pixels.length) {
+			return false;
+		}
+		return this.palette[this.pixels[rx][ry]];
 	}
 }
 
@@ -70,28 +105,66 @@ class Circle extends Sprite {
 }
 
 class Layer extends struct('objects', 'blendMode') {
+	get boundingBox() {
+		return new BoundingGroup(this.objects.map(object => object.boundingBox));
+	}
+
+	changedPixels() {
+		return flatMapUniq(this.objects, object => object.changedPixels());
+	}
+
+	getPixel(x, y) {
+		for(const object of this.objects) {
+			const pixel = object.getPixel(x, y);
+			if(pixel) return pixel;
+		}
+
+		return false;
+	}
+
+	drawPixel(ctx, {x, y}) {
+		ctx.globalCompositeOperation = this.blendMode || 'source-over';
+		ctx.fillStyle = this.getPixel(x, y) || 'transparent';
+		ctx.fillRect(x, y, 1, 1);
+	}
+}
+
+class Canvas extends struct('layers') {
+	get boundingBox() {
+		return new BoundingGroup(this.layers.map(layer => layer.boundingBox));
+	}
+
+	changedPixels() {
+		return flatMapUniq(this.layers, layer => layer.changedPixels());
+	}
+
 	draw(ctx) {
-		const oldBlendMode = ctx.globalCompositeOperation;
-		if(this.blendMode) ctx.globalCompositeOperation = this.blendMode;
-		this.objects.forEach(object => object.draw(ctx));
-		ctx.globalCompositeOperation = oldBlendMode;
+		this.changedPixels().forEach(pixel => {
+			this.drawPixel(ctx, pixel);
+		});
+	}
+
+	drawPixel(ctx, pixel) {
+		this.layers.forEach(layer => {
+			layer.drawPixel(ctx, pixel);
+		})
 	}
 }
 
 const circle1 = new Circle(new Point(95, 95), {
-	0: '#000000',
+	0: 'transparent',
 	1: '#FF0000',
 });
 
 const circle2 = new Circle(new Point(100, 95), {
-	0: '#000000',
+	0: 'transparent',
 	1: '#00FF00',
 });
 
-const layer = new Layer([
+const layer = new Canvas([
 	new Layer([circle1], false),
 	new Layer([circle2], 'lighter')
-], false);
+]);
 
 const canvas = document.createElement('canvas');
 canvas.width = canvas.height = 500;
@@ -101,10 +174,4 @@ document.body.style.background = '#000';
 
 const ctx = canvas.getContext('2d');
 
-function clear(ctx) {
-	ctx.fillStyle = 'black';
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-clear(ctx);
 layer.draw(ctx);
